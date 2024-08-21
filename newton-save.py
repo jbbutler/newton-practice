5df2aacce892d4e1ebb46fe41110f74161a02c49
Jimmy Butler <butlerj@berkeley.edu>
2024-08-21 16:47:44 -0700

    add some files

--- newton-save.py
+++ newton-save.py
@@ -1,26 +1,26 @@
 import numpy as np
 from functools import partial
 
-
 def optimize(start, fun):
-    delta = 10 ** (-6)
-    eps = 10 ** (-6)
+
+    delta = 10**(-3)
+    eps = 10**(-3)
     last_val = start
     first_der_func = partial(derivative, fun=fun, eps=eps)
     first_der = first_der_func(last_val)
     second_der = derivative(last_val, first_der_func, eps)
-    next_val = last_val - (first_der / second_der)
+    next_val = last_val - (first_der/second_der)
 
     while abs(next_val - last_val) >= delta:
         last_val = next_val
         first_der_func = partial(derivative, fun=fun, eps=eps)
         first_der = first_der_func(last_val)
         second_der = derivative(last_val, first_der_func, eps)
-        next_val = last_val - (first_der / second_der)
+        next_val = last_val - (first_der/second_der)
 
     return (next_val, fun(next_val))
 
 
 def derivative(x, fun, eps):
-    der = (fun(x + eps) - fun(x)) / eps
+    der = (fun(x+eps) - fun(x))/eps
     return der
